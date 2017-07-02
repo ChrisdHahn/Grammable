9db84825f0355d8ae451a4efc70f3e425fb11311
Chris Hahn <chrishahnd@gmail.com>
2017-07-02 00:25:07 +0000

    gram detail page

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -1,12 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe GramsController, type: :controller do
+  describe "grams#show action" do
+    it "should successfully show the page if the gram is found" do
+      gram = FactoryGirl.create(:gram)
+      get :show, params: { id: gram.id }
+      expect(response).to have_http_status(:success)
+    end
+
+    it "should return a 404 error if the gram is not found" do
+      get :show, params: { id: 'TACOCAT' }
+      expect(response).to have_http_status(:not_found)
+    end
+  end
+
   describe "grams#index action" do
     it "should successfully show the page" do
       get :index
       expect(response).to have_http_status(:success)
     end
   end
+
 
   describe "grams#new action" do
     it "should require users to be logged in" do
@@ -15,17 +29,14 @@ RSpec.describe GramsController, type: :controller do
     end
 
     it "should successfully show the new form" do
-      user = User.create(
-        email:                 'fakeuser@gmail.com',
-        password:              'secretPassword',
-        password_confirmation: 'secretPassword',
-        )
-        sign_in user
+      user = FactoryGirl.create(:user)
+      sign_in user
 
       get :new
       expect(response).to have_http_status(:success)
     end
   end
+
 
   describe "grams#create action" do
 
@@ -33,37 +44,28 @@ RSpec.describe GramsController, type: :controller do
       post :create, params: { gram: { message: "Hello" } }
       expect(response).to redirect_to new_user_session_path
     end
-    
+
     it "should successfully create a new gram in our database" do
-       user = User.create(
-        email:                 'fakeuser@gmail.com',
-        password:              'secretPassword',
-        password_confirmation: 'secretPassword',
-        )
-        sign_in user
+      user = FactoryGirl.create(:user)
+      sign_in user
 
-        post :create, params: { gram: { message: 'Hello!' } }
-        expect(response).to redirect_to root_path
+      post :create, params: { gram: { message: 'Hello!' } }
+      expect(response).to redirect_to root_path
 
-        gram = Gram.last
-        expect(gram.message).to eq("Hello!")
-        expect(gram.user).to eq(user)
+      gram = Gram.last
+      expect(gram.message).to eq("Hello!")
+      expect(gram.user).to eq(user)
     end
 
-  it "should properly deal with validation errors" do
-     user = User.create(
-        email:                 'fakeuser@gmail.com',
-        password:              'secretPassword',
-        password_confirmation: 'secretPassword',
-        )
-        sign_in user
+    it "should properly deal with validation errors" do
+      user = FactoryGirl.create(:user)
+      sign_in user
 
-      
       gram_count = Gram.count
       post :create, params: { gram: { message: '' } }
       expect(response).to have_http_status(:unprocessable_entity)
       expect(gram_count).to eq Gram.count
+    end
+
   end
- 
- end
 end
